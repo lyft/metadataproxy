c89513fdb13bea092ff373f34c58f46e456722bc
Ryan Lane <rlane@lyft.com>
2016-05-20 11:29:43 -0700

    Echo tag info

--- docker_push.sh
+++ docker_push.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+    echo "TAG is $TAG"
     docker build -f Dockerfile -t $REPO:$COMMIT .
     docker tag $REPO:$COMMIT $REPO:$TAG
     docker push $REPO
