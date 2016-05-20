a2cb1442877d73b262e748cb7d908f1b87a1be31
Ryan Lane <rlane@lyft.com>
2016-05-20 10:50:30 -0700

    Echo message when skipping docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 export REPO=lyft/metadataproxy
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
     docker build -f Dockerfile -t $REPO:$COMMIT .
     docker tag $REPO:$COMMIT $REPO:$TAG
     docker push $REPO
+else
+    echo 'Ignoring PR branch for docker push.'
 fi
