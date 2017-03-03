15bb2b6275afb226ec1dca2e361ce6d2d94726b5
Ryan Lane <rlane@lyft.com>
2017-03-03 11:46:19 -0800

    Be less paranoid about vars in run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 
-if [ "z${HOST}" = "z" ]; then
+if [ "z$HOST" = "z" ]; then
     HOST="0.0.0.0"
 fi
 
-if [ "z${PORT}" = "z" ]; then
+if [ "z$PORT" = "z" ]; then
     PORT=8000
 fi
 
-if [ "${DEBUG}" = "True" ]; then
+if [ "$DEBUG" = "True" ]; then
     LEVEL="debug"
 else
     LEVEL="warning"
 fi
 
-if [ "z${WORKERS}" = "z" ]; then
+if [ "z$WORKERS" = "z" ]; then
     WORKERS="1"
 fi
 
-/usr/local/bin/gunicorn metadataproxy:app --log-level ${LEVEL} --workers=${WORKERS} -k gevent -b ${HOST}:${PORT} --access-logfile=- --error-logfile=-
+/usr/local/bin/gunicorn metadataproxy:app --log-level $LEVEL --workers=$WORKERS -k gevent -b $HOST:$PORT --access-logfile=- --error-logfile=-
