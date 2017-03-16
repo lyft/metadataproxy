96b826b398240140bbeef7e12cb9cc7b3c1fddac
Caroline Beauchamp <caroline@chillibean.tv>
2017-03-16 15:28:03 +0000

    only use app after defining it :)

--- metadataproxy/__init__.py
+++ metadataproxy/__init__.py
@@ -8,15 +8,17 @@ from logging import StreamHandler
 
 log = logging.getLogger(__name__)
 stream_handler = StreamHandler(stream=sys.stdout)
-if app.config['DEBUG']:
-    stream_handler.setLevel(logging.DEBUG)
-else:
-    stream_handler.setLevel(logging.ERROR)
     
 
 app = Flask(__name__)
 app.config.from_object(settings)
 app.debug = app.config['DEBUG']
+
+if app.config['DEBUG']:
+    stream_handler.setLevel(logging.DEBUG)
+else:
+    stream_handler.setLevel(logging.ERROR)
+
 app.logger.addHandler(stream_handler)
 
 if app.config['MOCK_API']:
