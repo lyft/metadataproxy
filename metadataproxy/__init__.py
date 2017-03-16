e3beb553accd827136cf4f61d33a6ef174a816dd
Caroline Beauchamp <caroline@chillibean.tv>
2017-03-16 10:27:37 +0000

    Add in proper logging

--- metadataproxy/__init__.py
+++ metadataproxy/__init__.py
@@ -1,14 +1,19 @@
 import logging
+import sys
 
 from flask import Flask
 
 from metadataproxy import settings
+from logging import StreamHandler
 
 log = logging.getLogger(__name__)
+stream_handler = StreamHandler(stream=sys.stdout)
+stream_handler.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object(settings)
 app.debug = app.config['DEBUG']
+app.logger.addHandler(stream_handler)
 
 if app.config['MOCK_API']:
     from metadataproxy.routes import mock  # NOQA
